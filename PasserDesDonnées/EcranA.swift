0c4e47988525a7c53c7304ce9e29048525dc2217
Aude <audechevalie@gmail.com>
2018-10-09 08:37:42 +0200

    Premier commit

---
+++ PasserDesDonnées/EcranA.swift
@@ -0,0 +1,22 @@
+//
+//  EcranA.swift
+//  PasserDesDonnées
+//
+//  Created by CHEVALIER on 09/10/2018.
+//  Copyright © 2018 CHEVALIER. All rights reserved.
+//
+
+import UIKit
+
+class EcranA: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+    
+
+
+}
---
+++ PasserDesDonnées/EcranB.swift
@@ -0,0 +1,23 @@
+//
+//  EcranB.swift
+//  PasserDesDonnées
+//
+//  Created by CHEVALIER on 09/10/2018.
+//  Copyright © 2018 CHEVALIER. All rights reserved.
+//
+
+import UIKit
+
+class EcranB: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+     
+    }
+
+
+    
+
+
+}
